479d4c923eca699f564e326e1603a352e43e8a63
Bipasha88 <dolasaha88@gmail.com>
2021-03-29 10:08:19 -0700

    918B submission accepted

---
+++ 918B.php
@@ -0,0 +1,37 @@
+<?php
+
+$parameter=readline();
+$parameter_line=explode(" ",$parameter);
+
+$n=$parameter_line[0];
+$m=$parameter_line[1];
+$servers=[];
+$commands=[];
+
+for($i=0;$i<$n;$i++){
+    $server=readline();
+    $server_line=explode(" ",$server);
+    for($j=0;$j<2;$j++){
+        $servers[$i][$j]=$server_line[$j];
+    }
+}
+
+for($i=0;$i<$m;$i++){
+    $command=readline();
+    $tcommand=trim($command,";");
+    $command_line=explode(" ",$tcommand);
+    for($j=0;$j<2;$j++){
+        $commands[$i][$j]=$command_line[$j];
+    }
+}
+
+for($i=0;$i<$m;$i++){
+    for($j=0;$j<$n;$j++){
+        if($commands[$i][1]==$servers[$j][1]){
+            echo $commands[$i][0]." ".$commands[$i][1]."; #".$servers[$j][0];
+            echo "\n";
+        }
+    }
+}
+
+
